53d1d4fed0db10f73d6ba5b389a60f4c57090b5c
benkldev <55231890+BenKLDEV@users.noreply.github.com>
2020-05-03 21:51:20 +0200

    Updates

---
+++ precache-manifest.ece6b727b57022d79b6eeb75acbc9e56.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b828a86ac99b3b340b0c36f4c365b47f",
+    "url": "/democrat-quiz-react/index.html"
+  },
+  {
+    "revision": "93081878b41ec36b5ca5",
+    "url": "/democrat-quiz-react/static/css/main.613f737b.chunk.css"
+  },
+  {
+    "revision": "6d0f4a51d2bb66fa007e",
+    "url": "/democrat-quiz-react/static/js/2.c90619e0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/democrat-quiz-react/static/js/2.c90619e0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "93081878b41ec36b5ca5",
+    "url": "/democrat-quiz-react/static/js/main.fe0be434.chunk.js"
+  },
+  {
+    "revision": "90a787b82265fa78d49e",
+    "url": "/democrat-quiz-react/static/js/runtime-main.ebd8cee1.js"
+  }
+]);
