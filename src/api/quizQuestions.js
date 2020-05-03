4a7abf763b759e298e3732b26d70962f2ebae8a0
benkldev <55231890+BenKLDEV@users.noreply.github.com>
2020-05-03 23:25:54 +0200

    democrat-quiz-react

---
+++ src/api/quizQuestions.js
@@ -0,0 +1,140 @@
+const quizQuestions = [
+  {
+    question: "Vilken ledarstil gillar du mest?",
+    answers: [
+      {
+        type: "Demokrati",
+        content: "Att låta hela gruppen bestämma"
+      },
+      {
+        type: "Oligarki",
+        content: "Att låta ett fåtal personer bestämma"
+      },
+      {
+        type: "Diktatur",
+        content: "Att jag själv eller någon annan bestämmer allt"
+      }
+    ]
+  },
+  {
+    question: "Är ordet demokrati ett ord som du ogillar?",
+    answers: [
+      {
+        type: "Demokrati",
+        content: "Verkligen inte"
+      },
+      {
+        type: "Oligarki",
+        content: "Lite halvt"
+      },
+      {
+        type: "Diktatur",
+        content: "Verkligen"
+      }
+    ]
+  },
+  {
+    question: "Tycker du att alla partier ska få komma till tals i riksdagen?",
+    answers: [
+      {
+        type: "Demokrati",
+        content: "Absolut"
+      },
+      {
+        type: "Oligarki",
+        content: "Inte riktigt. Bara ett fåtal."
+      },
+      {
+        type: "Diktatur",
+        content: "Nej. Bara ett parti ska få komma till tals."
+      }
+    ]
+  },
+  {
+    question: "Skulle du vilja att Sverige var ett enmansvälde?",
+    answers: [
+      {
+        type: "Demokrati",
+        content: "Absolute inte"
+      },
+      {
+        type: "Oligarki",
+        content: "Nej. Men en oligarki."
+      },
+      {
+        type: "Diktatur",
+        content: "Det låter bra. Det passar mig."
+      }
+    ]
+  },
+  {
+    question: "Nämn en ledare vars ledarstil du tycker om",
+    answers: [
+      {
+        type: "Demokrati",
+        content: "Isabella Lövin"
+      },
+      {
+        type: "Oligarki",
+        content: "Putin"
+      },
+      {
+        type: "Diktatur",
+        content: "Adolf Hitler"
+      }
+    ]
+  },
+  {
+    question: "Ska folk få demonstrera i ditt land?",
+    answers: [
+      {
+        type: "Demokrati",
+        content: "Ja verkligen! Det är en mänsklig rättighet"
+      },
+      {
+        type: "Oligarki",
+        content: "Nja, om dem inte blir ett hot mot oss så är det ok. Annars inte."
+      },
+      {
+        type: "Diktatur",
+        content: "Bara om det är vad jag tillåter."
+      }
+    ]
+  },
+  {
+    question: "Ska folket ha mer makt i ditt land?",
+    answers: [
+      {
+        type: "Demokrati",
+        content: "Verkligen! Utan tvekan."
+      },
+      {
+        type: "Oligarki",
+        content: "Så länge det inte störa våra lilla inflyteserika grupp."
+      },
+      {
+        type: "Diktatur",
+        content: "Om det följer mitt kommand. Annars kommer det inte på fråga."
+      }
+    ]
+  },
+  {
+    question: "Vem av dessa ledare är mest demokratisk enligt dig?",
+    answers: [
+      {
+        type: "Demokrati",
+        content: "Barrack Obama"
+      },
+      {
+        type: "Oligarki",
+        content: "Putin"
+      },
+      {
+        type: "Diktatur",
+        content: "Kim Jong Un"
+      }
+    ]
+  }
+];
+  
+export default quizQuestions;
